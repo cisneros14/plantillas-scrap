d61db96d2863f4fd4cf8c4a7aacd44b00ff5d7b7
Carlos Cisneros <cisnerosgranda14@gmail.com>
2026-02-06 10:35:43 -0500

    v3

--- src/app/(home)/catalogo/page.tsx
+++ src/app/(home)/catalogo/page.tsx
@@ -39,7 +39,7 @@ export default async function CatalogoPage({
         <div className="mb-4 items-end justify-between space-y-4 sm:flex sm:space-y-0 md:mb-8">
           <div>
             <h2 className="mt-3 text-xl font-semibold text-gray-900 dark:text-white sm:text-2xl">
-              Catálogo de Electrónica
+              Catálogo
             </h2>
           </div>
         </div>
