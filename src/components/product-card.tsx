9ffcd4777cc00bd00624ff0d4a57c6be1b6bbe10
Carlos Cisneros <cisnerosgranda14@gmail.com>
2026-02-06 15:04:57 -0500

    mancasa v2

--- src/components/product-card.tsx
+++ src/components/product-card.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import { Card } from "@/components/ui/card";
 import { Button } from "./ui/button";
+import { HeartIcon } from "lucide-react";
 
 interface ProductFeature {
   label: string;
@@ -193,21 +194,7 @@ export function ProductCard({
               className="rounded-lg p-2 text-gray-500 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
             >
               <span className="sr-only">Añadir a favoritos</span>
-              <svg
-                className="h-5 w-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  stroke="currentColor"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z"
-                />
-              </svg>
+              <HeartIcon className="h-5 w-5 text-secondary" />
             </button>
             <div
               id={`tooltip-add-to-favorites-${product.id}`}
